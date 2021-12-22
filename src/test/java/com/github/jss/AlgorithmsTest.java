a2d883fc2f27f593877d7c31b3330329e99d085a
Piotr Melkowski <piotr.melkowski@gmail.com>
2021-12-22 20:27:57 +0100

    Update AlgorithmsTest

--- src/test/java/com/github/jss/AlgorithmsTest.java
+++ src/test/java/com/github/jss/AlgorithmsTest.java
@@ -1,8 +1,11 @@
 package com.github.jss;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class AlgorithmsTest {
@@ -38,41 +41,42 @@ public class AlgorithmsTest {
         assertFalse(Algorithms.getAlgorithms(serviceType).isEmpty());
     }
 
-    @Test
-    public void testGetCertificateProviderNames() {
-        for (String algorithm : Algorithms.getCertificateAlgorithms()) {
-            assertFalse(Algorithms.getCertificateProviderNames(algorithm).isEmpty());
-        }
-    }
-
-    @Test
-    public void testGetKeyProviderNames() {
-        for (String algorithm : Algorithms.getKeyAlgorithms()) {
-            assertFalse(Algorithms.getKeyProviderNames(algorithm).isEmpty());
-        }
+    @ParameterizedTest
+    @MethodSource("com.github.jss.Algorithms#getCertificateAlgorithms")
+    public void testGetCertificateProviderNames(String algorithm) {
+        assertFalse(Algorithms.getCertificateProviderNames(algorithm).isEmpty());
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {
-        "AlgorithmParameters",
-        "AlgorithmParameterGenerator",
-        "CertificateFactory",
-        "CertPathBuilder",
-        "CertPathValidator",
-        "CertStore",
-        "Configuration",
-        "KeyFactory",
-        "KeyPairGenerator",
-        "KeyStore",
-        "MessageDigest",
-        "Policy",
-        "SecureRandom",
-        "Signature"
-    })
-    public void testGetProviderNames(String serviceType) {
-        for (String algorithm : Algorithms.getAlgorithms(serviceType)) {
-            assertFalse(Algorithms.getProviderNames(serviceType, algorithm).isEmpty());
-        }
+    @MethodSource("com.github.jss.Algorithms#getKeyAlgorithms")
+    public void testGetKeyProviderNames(String algorithm) {
+        assertFalse(Algorithms.getKeyProviderNames(algorithm).isEmpty());
+    }
+
+    @ParameterizedTest
+    @MethodSource("getServicesAndAlgorithms")
+    public void testGetProviderNames(String serviceType, String algorithm) {
+        assertFalse(Algorithms.getProviderNames(serviceType, algorithm).isEmpty());
+    }
+
+    private static Stream<Arguments> getServicesAndAlgorithms() {
+        return Stream.of(
+                "AlgorithmParameters",
+                "AlgorithmParameterGenerator",
+                "CertificateFactory",
+                "CertPathBuilder",
+                "CertPathValidator",
+                "CertStore",
+                "Configuration",
+                "KeyFactory",
+                "KeyPairGenerator",
+                "KeyStore",
+                "MessageDigest",
+                "Policy",
+                "SecureRandom",
+                "Signature")
+            .flatMap(serviceType -> Algorithms.getAlgorithms(serviceType).stream()
+                .map(algorithm -> Arguments.of(serviceType, algorithm)));
     }
 
 }
